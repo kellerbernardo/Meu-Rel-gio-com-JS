319d6d4e2c82da6de6430279a3956af753321720
Bernardo <bernardox34@gmail.com>
2023-02-14 21:13:59 -0300

    relogio finalizado

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let secondsHand = document.querySelector(".hand.seconds");
+let minutesHand = document.querySelector(".hand.minutes");
+let hoursHand   = document.querySelector(".hand.hours");
+
+const setClock = () => {
+    const currentDate = new Date();
+
+    const secondsPercentage = currentDate.getSeconds() / 60;
+    const minutesPercentage = (secondsPercentage + currentDate.getMinutes()) / 60;
+    const hoursPercentage   = (minutesPercentage + currentDate.getHours()) / 12;
+    
+    setRotation(secondsPercentage, secondsHand);
+    setRotation(minutesPercentage, minutesHand);
+    setRotation(hoursPercentage,   hoursHand);
+
+}
+
+const setRotation = (rotationPercentage, element) => {
+    element.style.setProperty("--rotation", rotationPercentage * 360);
+}
+
+setClock()
+
+setInterval(setClock, 1000);
